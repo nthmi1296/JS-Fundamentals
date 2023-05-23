c93c6602bad367b523076c4c8da689bbec861128
nthmi1296 <nthmi1296@gmail.com>
2023-05-23 12:35:27 +0700

    1-6

---
+++ lesson2.js
@@ -0,0 +1,7 @@
+// Concatination and Interpolation
+var price = 50
+var itemName = "Cup"
+var messageToPrint1 = "The price for your " + itemName +" is "+ price +" dollars"
+var messageToPrint2 = `Then price for your ${itemName} is ${price} dollars`
+console.log(messageToPrint1)
+console.log(messageToPrint2)
---
+++ lesson6.js
@@ -0,0 +1,30 @@
+// Condition statement
+/*
+if(condition){
+    // execute some code
+} else{
+    // execute some code
+}
+*/
+// If hour between 6 and 12 print "Good Morning"
+// If hour between 12 and 18 print "Good Afternoon"
+// Otherwise: Good Evening
+
+var hour = 9
+
+// if (hour >= 6 && hour <12){
+//     console.log('Good Morning')
+// } else if(hour >= 12 && hour < 18){
+//     console.log('Good Afternoon')
+// } else {
+//     console.log('Good Evening')
+// }
+
+var ageIsMoreThanEightTeen = true
+var isUSCitizen = true
+
+if(ageIsMoreThanEightTeen && isUSCitizen){
+    console.log('Customer is eligible for DL')
+} else{
+    console.log('Customer is NOT eligible for DL')
+}
